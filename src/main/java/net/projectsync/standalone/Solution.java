080674169f0737d8792d3c57697d427fee89abba
mohankumarn24 <mohankumar.n24@outlook.com>
2025-10-19 20:38:34 +0530

    Add file IO changes

--- src/main/java/net/projectsync/standalone/Solution.java
+++ src/main/java/net/projectsync/standalone/Solution.java
@@ -1,9 +1,19 @@
 package net.projectsync.standalone;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 public class Solution {
 
     public static void main(String[] args) {
 
-        System.out.println("hello, world");
+        List<Integer> numList = Arrays.asList(1, 2, 3, 4, 5);
+        Integer[] nums = numList.toArray(Integer[]::new);
+        for (int num : nums) {
+            System.out.println(num);
+        }
+
     }
 }
